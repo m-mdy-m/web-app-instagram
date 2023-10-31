600c44d060b23298de002220a9b553805cff968e
mahdi <mahdimamashli1383@gmail.com>
2023-10-31 21:12:19 +0330

    create aside and finish

--- components/PageDir/PageDir.js
+++ components/PageDir/PageDir.js
@@ -1,13 +1,13 @@
 const template = document.createElement("template");
 template.innerHTML = `
 	<link rel="stylesheet" href="style/style.css"/>
-    <div class="flex justify-start items-start w-full h-auto mt-2">
-		<img src="" alt="profile" class="w-14 h-14 rounded-full border border-solid opacity-0 animate-opacityAnimation border-x-cyan-950 border-y-cyan-700 hover:border-cyan-700 transition-all duration-400 hover:border-2 ">
-		<div class="flex justify-center items-start flex-col  pl-2">
-			<h3 class=" font-mavisBold text-[#1d1d22]"></h3>
-			<p class="text-[#b0b0b2] font-aktivLight text-[.5rem] -mt-2"></p>
-		</div>
-	</div>`;
+    <div class="flex justify-start items-start h-auto mt-2 pl-2 cursor-pointer hover:bg-black w-full transition duration-500 rounded-tr-[2rem] rounded-br-[2rem]">
+							<img src="" alt="profile" class="w-14 h-14  rounded-full border border-solid opacity-0 animate-opacityAnimation border-x-cyan-950 border-y-cyan-700 hover:border-cyan-700 transition-all duration-400 hover:border-2 ">
+							<div class="flex justify-center items-start flex-col  pl-2">
+								<h3 class=" font-mavisBold text-PrimaryText">m__mdy__m</h3>
+								<p class="text-[#89898b] font-aktivLight text-sm -mt-2">hi</p>
+							</div>
+						</div>`;
 class pageDir extends HTMLElement {
 	constructor() {
 		super();
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,16 @@ module.exports = {
 			},
 			animation: {
 				opacityAnimation: "opacityAnimation 2s forwards ;",
-				MoveToRight : 'MoveToRight 0.2s forwards 3s cubic-bezier(0,-0.67, 0.13, 1.49);'
+				MoveToRight:
+					"MoveToRight 0.2s forwards 3s cubic-bezier(0,-0.67, 0.13, 1.49);",
+			},
+			colors: {
+				PrimaryText: {
+					DEFAULT: "#f0edf5da",
+					100: "#f0edf5d3",
+					200: "#f0edf5d3",
+					300: "#89898b",
+				},
 			},
 		},
 		container: {
