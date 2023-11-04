544b067069baacddde143c1fd2e2755b22425c2e
mahdi <mahdimamashli1383@gmail.com>
2023-11-04 18:47:24 +0330

    change image story

--- components/story/story.js
+++ components/story/story.js
@@ -3,7 +3,7 @@ template.innerHTML = `
 	<link rel="stylesheet" href="style/style.css" />
 	<div class="lg:h-48 lg:w-[85px] sm:h-full sm:w-[50px] mx-3 flex flex-col justify-between items-center  relative font-aktivLight bgImage lowercase shadowBefore rounded-lg   opacity-100 odd:animate-MoveToBottom even:animate-MoveToTop ">
 	<figure class="lg:h-full lg:w-full sm:w-8 sm:h-8 flex flex-col justify-between items-center ">
-		<img src="images/image/image_story.jpg" alt="" class="lg:w-16 lg:h-16 rounded-full sm:w-8 sm:h-8 storyLine cursor-pointer mt-2 hover:scale-[1.2] transition-all duration-1000 object-cover">
+		<img src="images/image/image_story.jpg" alt="" class="lg:w-16 lg:h-16 rounded-full sm:w-8 sm:h-8 storyLine object-cover object-bottom cursor-pointer mt-2 hover:scale-[1.2] transition-all duration-1000">
 	</figure>
 	<h3 class=" lg:text-xs text-gray-100  text-center lg:p-0 lg:pb-4  sm:p-4 sm:text-[10px] cursor-pointer  ">m--m--m</h3>
 </div>`;
